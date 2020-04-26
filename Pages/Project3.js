438bfeaf7c75aa69da1b7b86148781ed5b868153
lizshavrick <shave343@newschool.edu>
2020-04-26 12:23:34 -0400

    u

---
+++ Pages/Project3.js
@@ -0,0 +1,8 @@
+<script>
+    function myClick(){
+        text = "<b><i><br><br>You can save a ton of money by drinking tap or filtered water,  instead of wasting hundreds of dollars on bottled water. Did you know the average person spends $100+ per person every year! The good thing is its not your fault; many people are inticed by clever advertising, and thinking it has neutrisional values that other water does not contain. The truth is Big companies like Nestle and Coca'cola are simply profiting off making you think youre making a healthy choice. Bottled water is only tested once a weak for contamination and germs, and is no better than your average tap water. Honestly, if you really wanted to make a healthier decision try filtered water. You could argue that you still have a water bill dont you? well, according to STATISTIC BRAIN,'the total cost of a monthly water bill if tap cost as much as the cheapest watter bottle would be $9000!'  </i></b>";
+        document.getElementById('heading1').innerHTML = document.getElementById('heading1').innerHTML + text;
+        document.getElementById("heading1").style.textAlign="left";
+        document.getElementById("heading1").style.marginLeft="20px";
+    }
+</script>
